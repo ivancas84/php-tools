71ace39c59e9da06aff5e3823acb8b727ac6db84
ivancas84 <ivancas84@gmail.com>
2020-04-08 09:29:22 -0300

    Actualizar archivo de configuracion

--- structure/config/config.php
+++ structure/config/config.php
@@ -1,32 +1,55 @@
 <?php
-ini_set("display_errors", 1);
-ini_set('date.timezone', 'America/Argentina/Buenos_Aires');
-session_set_cookie_params(7200, '/', 'localhost'); //testing
-//session_set_cookie_params(7200, '/', '.dominio.com.ar'); //produccion
+
+//ini_set("display_errors", 1);  //testing
 //session_id(1); //testing
+//ini_set('date.timezone', 'America/Argentina/Buenos_Aires'); //testing
+//session_set_cookie_params(7200, '/', 'localhost'); //testing
+//session_set_cookie_params(7200, '/', '.dominio.com.ar'); //testing
 
-define("SYS_NAME", "name"); //nombre del modulo de administracion
-define("PATH_SYS", "path"); //path correspondiente al módulo de administración
 
-//constantes de acceso a la base de datos
+/**
+ * Identificacion
+ */
+define("SYS_NAME", "System Name"); //nombre del modulo de administracion
+define("PATH_SYS", "path/to/structure"); //path correspondiente al módulo de administración
+
+/**
+ * Acceso a la base de datos
+ */
 define("DATA_DBNAME", "");
 define("DATA_USER", "");
 define("DATA_PASS", "");
 define("DATA_HOST", "");
 define("DATA_DBMS", ""); //mysql || pgsql
-
 define("DATA_SCHEMA", "");
-define("DISABLE_ENTITIES", "");
 
-//raiz del modulo de administración
+/**
+ * Acceso a la base de datos de transacciones
+ */
+define("TXN_DBNAME", "");
+define("TXN_USER", "");
+define("TXN_PASS", "");
+define("TXN_HOST", "");
+define("TXN_DBMS", ""); //mysql || pgsql
+define("TXN_SCHEMA", "");
+
+/**
+ * Valores especiales
+ */
+define("DISABLE_ENTITIES", ""); //entidades de la base de datos que seran deshabilitadas
+define("UNDEFINED", "~"); //valor no definido
+define("DEFAULT_VALUE", "^"); //valor por defecto
+
+/**
+ * Raiz del modulo de administracion
+ */
 define("PATH_HTTP" , "http://" . $_SERVER["SERVER_NAME"] . "/" . PATH_SYS . "/");
 define("PATH_ROOT" ,  $_SERVER["DOCUMENT_ROOT"] . "/" . PATH_SYS . "/");
 //define("PATH_LOGIN", "http://" . $_SERVER["SERVER_NAME"] . "/" . PATH_SYS . "/login/"); //path correspondiente al módulo de login
 
-define("UNDEFINED", "~"); //constante especial para indicar valor no definido, utilizada especialmente en las clases de manipulacion de valores
-define("DEFAULT_VALUE", "^");
-
-//definición de rutas de inclusión
+/**
+ * Rutas de inclusion
+ */
 set_include_path(get_include_path()
   . PATH_SEPARATOR . $_SERVER["DOCUMENT_ROOT"] . "/" . PATH_SYS . "/src"
   . PATH_SEPARATOR . $_SERVER["DOCUMENT_ROOT"] . "/" . PATH_SYS . "/src/core"
