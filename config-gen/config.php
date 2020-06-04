194f3b6e195beb4b3a7b70d604663536105190a1
ivancas84 <ivancas84@gmail.com>
2020-06-04 09:10:53 -0300

    Agregar gitignore y modificar archivo de configuracion de generacion

--- config-gen/config.php
+++ config-gen/config.php
@@ -3,9 +3,9 @@
 require_once($_SERVER["DOCUMENT_ROOT"] . "/config/config.php");
 
 define("PATH_CORE", "path/to/core/src");
-define("PATH_SITE", "path/to/site/src");
-define("PATH_DOC", "path/to/site/doc");
-define("PATH_GEN", "path/to/site/gen");
+define("PATH_SITE", "path/to/this/site/src");
+define("PATH_DOC", "path/to/gen/site/doc");
+define("PATH_GEN", "path/to/gen/site/src");
 
 set_include_path(
   . PATH_SEPARATOR . $_SERVER["DOCUMENT_ROOT"] . "/" . PATH_SITE
