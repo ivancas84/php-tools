1684bf3b82f7c5e68403c49eae1ed498464b70c3
unknown <ivancas84@gmail.com>
2021-01-21 10:46:48 -0300

    Correcciones minimas

--- php-structure-gen/config/config.php
+++ php-structure-gen/config/config.php
@@ -3,7 +3,7 @@
 require_once($_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "magistrados-estructura" . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "config.php");
 
 set_include_path(
-  $_SERVER["DOCUMENT_ROOT"] . "" . DIRECTORY_SEPARATOR . "php-main-gen" . DIRECTORY_SEPARATOR . "src"
+  $_SERVER["DOCUMENT_ROOT"] . "" . DIRECTORY_SEPARATOR . "php-structure-gen" . DIRECTORY_SEPARATOR . "src"
   . PATH_SEPARATOR . $_SERVER["DOCUMENT_ROOT"] . "" . DIRECTORY_SEPARATOR . "php-main-gen" . DIRECTORY_SEPARATOR . "core" . DIRECTORY_SEPARATOR . "src"
   . PATH_SEPARATOR . get_include_path()
 );
